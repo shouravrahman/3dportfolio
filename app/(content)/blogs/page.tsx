2d95e0da3d28d33669e17e281e95f8ce5cf9725e
shourav <shouravrahman006@gmail.com>
2024-09-15 01:02:13 +0600

    article page

---
+++ app/(content)/blogs/page.tsx
@@ -0,0 +1,42 @@
+
+import { Metadata } from "next";
+import { WEBSITE_NAME, META_SEO_KEYWORDS } from "@/constants/appSetup";
+import { SanityDocument } from "@sanity/client";
+import { postsQuery } from "@/sanity/lib/queries";
+import { sanityFetch } from "@/sanity/lib/sanityFetch";
+import Text from "@/components/Article/Text";
+import HomeArticles from "@/components/Article/HomeArticles";
+import { SimpleLayout } from "@/components/Article/SimpleLayout";
+
+export const metadata: Metadata = {
+   title: "Articles",
+   description: `Dive into insightful articles by ${WEBSITE_NAME}. Explore tech, coding, and innovation through our expert perspectives.`,
+   keywords: META_SEO_KEYWORDS,
+};
+
+const Article = async () => {
+   const articles = await sanityFetch<SanityDocument>({
+      query: postsQuery,
+   });
+
+   return (
+
+      <div className={"flex flex-col flex-wrap w-full"}>
+         {articles?.length > 0 ? (
+            <HomeArticles
+               isArchive={false}
+               noOfArticle={6}
+               articles={articles}
+               isSeries={false}
+               isExternal={false}
+            />
+         ) : (
+            <p>No Article Found</p>
+         )}
+      </div>
+
+
+   );
+};
+
+export default Article;
