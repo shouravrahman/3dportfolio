b329ab1c784fc840968a2e231bb3366cff5336b5
shourav <shouravrahman006@gmail.com>
2024-09-15 01:01:55 +0600

    main page

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,32 @@
 
+import About from "@/components/About";
+import HomeArticles from "@/components/Article/HomeArticles";
+import Contact from "@/components/Contact";
 import Hero from "@/components/Hero";
+import Projects from "@/components/Projects";
+import Testimonials from "@/components/Testimonials";
+import WorkExperience from "@/components/WorkExperience";
+import { postsQuery } from "@/sanity/lib/queries";
+import { sanityFetch } from "@/sanity/lib/sanityFetch";
+import { SanityDocument } from "next-sanity";
 
 
 
-export default function Home() {
+export default async function Home() {
+
+   const articles = await sanityFetch<SanityDocument>({
+      query: postsQuery,
+   });
   return (
-     <main className="">
+     <main className=" mx-auto overflow-hidden">
         <Hero />
+        <About />
+        <Projects />
+        <HomeArticles articles={articles} />
+        <Testimonials />
+        <WorkExperience />
+        <Contact />
+
      </main>
 
   );
