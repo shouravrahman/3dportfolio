5902beb1747118570e91b93a69f842c4733df9a4
shourav <shouravrahman006@gmail.com>
2024-09-15 01:01:33 +0600

    root layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 
 import "./globals.css";
 import Navbar from "@/components/Navbar";
+import Footer from "@/components/Footer";
 
 
 export const metadata: Metadata = {
@@ -21,6 +22,7 @@ export default function RootLayout({
       >
            <Navbar />
         {children}
+           <Footer />
       </body>
     </html>
   );
