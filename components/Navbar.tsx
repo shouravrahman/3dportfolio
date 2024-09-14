dc5aa4bce0f5ad4a53d5afe00b02145d9b85421c
shourav <shouravrahman006@gmail.com>
2024-09-15 01:04:52 +0600

    simple navbar

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,5 +1,6 @@
 "use client"
 import { navLinks } from '@/constants';
+import Link from 'next/link';
 import { useState } from 'react';
 
 
@@ -7,9 +8,9 @@ const NavItems = ({ onClick = () => { } }) => (
    <ul className="nav-ul">
       {navLinks.map((item) => (
          <li key={item.id} className="nav-li">
-            <a href={item.href} className="nav-li_a" onClick={onClick}>
+            <Link href={item.href} className="nav-li_a" onClick={onClick}>
                {item.name}
-            </a>
+            </Link>
          </li>
       ))}
    </ul>
@@ -25,9 +26,9 @@ const Navbar = () => {
       <header className="fixed top-0 left-0 right-0 z-50 bg-black/90">
          <div className="max-w-7xl mx-auto">
             <div className="flex justify-between items-center py-5 mx-auto c-space">
-               <a href="/" className="text-neutral-400 font-bold text-xl hover:text-white transition-colors">
+               <Link href="/" className="text-neutral-400 font-bold text-xl hover:text-white transition-colors">
                   Shourav
-               </a>
+               </Link>
 
                <button
                   onClick={toggleMenu}
