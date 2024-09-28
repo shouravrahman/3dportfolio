40166c17622213768847de8d17abdefd6c8767f5
shourav <shouravrahman006@gmail.com>
2024-09-29 02:12:12 +0600

    speed insights and audit

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,4 @@
-
+import { SpeedInsights } from "@vercel/speed-insights/next";
 import About from "@/components/About";
 import HomeArticles from "@/components/Article/HomeArticles";
 import Contact from "@/components/Contact";
